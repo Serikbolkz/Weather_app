45367ba74f64d3e2594477cb2a6ab870d1b56146
SerikbolKz <serikboltalgatuly1@gmail.com>
2025-05-16 22:15:50 +0500

    Database moved to PostgreSQL

--- homepage/views.py
+++ homepage/views.py
@@ -1,36 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from django.conf import settings
 from .models import City
 from .forms import CityForm
+from django.contrib import messages
 
 def index(request):
     WEATHER_API_KEY = settings.WEATHER_API_KEY
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+WEATHER_API_KEY
-
-    cities = City.objects.all()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + WEATHER_API_KEY
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        if form.is_valid():
+            city_name = form.cleaned_data['name']
+            api_response = requests.get(url.format(city_name)).json()
 
-    form = CityForm()
+            if api_response.get('main'):
+                form.save()
+                return redirect('index')
+            else:
+                messages.error(request,'City not found')
+                return redirect('index')
+    else:
+        form = CityForm()
 
+    cities = City.objects.all()
     weather_data = []
 
     for city in cities:
+        city_weather = requests.get(url.format(city.name)).json()
+        if city_weather.get('main'):
+            weather = {
+                'city': city.name,
+                'temperature': city_weather['main']['temp'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon']
+            }
+            weather_data.append(weather)
 
-        city_weather = requests.get(url.format(city)).json() 
-
-        weather = {
-            'city' : city,
-            'temperature' : city_weather['main']['temp'],
-            'description' : city_weather['weather'][0]['description'],
-            'icon' : city_weather['weather'][0]['icon']
-        }
-
-        weather_data.append(weather)
-
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {
+        'form': form, 
+        'weather_data': weather_data
+    }
 
     return render(request, 'index.html', context)
+
+
--- weather_app/settings.py
+++ weather_app/settings.py
@@ -11,6 +11,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -51,14 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'weather_app.wsgi.application'
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
